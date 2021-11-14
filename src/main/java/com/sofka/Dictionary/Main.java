705c390675e5b3522ad9590f2c2b1306bef88f83
EmmilioV <emmiliov.p@gmail.com>
2021-11-14 02:14:06 -0500

    create menú for dictionary options

---
+++ src/main/java/com/sofka/Dictionary/Main.java
@@ -0,0 +1,11 @@
+package com.sofka.Dictionary;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("    MENÚ    \n" +
+                "Digite el número según la acción que desee realizar: \n" +
+                "1. Registrar una palabra en español con su traducción al inglés \n" +
+                "2. Consultar todo el diccionario \n" +
+                "3. Consultar traducciond e una palabra en español");
+    }
+}
---
+++ src/main/java/com/sofka/Dictionary/Options.java
@@ -0,0 +1,4 @@
+package com.sofka.Dictionary;
+
+public class Options {
+}

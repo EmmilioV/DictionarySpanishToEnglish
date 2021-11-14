d0fa36b3a4fa4e4c4a9775c94ab9ae5e47732548
EmmilioV <emmiliov.p@gmail.com>
2021-11-14 02:32:09 -0500

    se creo el menu con joptionpane y switch case

--- src/main/java/com/sofka/Dictionary/Main.java
+++ src/main/java/com/sofka/Dictionary/Main.java
@@ -1,11 +1,34 @@
 package com.sofka.Dictionary;
 
+import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("    MENÚ    \n" +
-                "Digite el número según la acción que desee realizar: \n" +
-                "1. Registrar una palabra en español con su traducción al inglés \n" +
-                "2. Consultar todo el diccionario \n" +
-                "3. Consultar traducciond e una palabra en español");
+        int option = 0;
+        InputStreamReader in = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(in);
+
+        do
+        {
+            option = Integer.parseInt(JOptionPane.showInputDialog("    MENU    \n" +
+                    "Digite el numero segun la accion que desee realizar: \n" +
+                    "1. Registrar una palabra en español con su traduccion al ingles \n" +
+                    "2. Consultar todo el diccionario \n" +
+                    "3. Consultar traduccion de una palabra en español"));
+            switch (option)
+            {
+                case 1 : 
+                    break;
+                case 2 :
+                    break;
+                case  3 :
+                    break;
+                default :
+                    option = 0;
+            }
+        }while(option == 0);
     }
+
 }
